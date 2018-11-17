8a21615628d8c71b804e9e417cc54f455f1534ec
Yq7 <songyuqi7@gmail.com>
2018-11-17 19:12:15 +0800

    add random_utils.py

---
+++ utils/random_utils.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import random
+
+
+# 产生随机数组
+def random_int_list(start=0, stop=1000, length=10):
+    start, stop = (int(start), int(stop)) if start <= stop else (int(stop), int(start))
+    length = int(abs(length)) if length else 0
+    random_list = []
+    for i in range(length):
+        random_list.append(random.randint(start, stop))
+    return random_list
