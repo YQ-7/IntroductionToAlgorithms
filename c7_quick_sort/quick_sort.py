003b2b501636fac39171fc24465a473db95d490d
YQ-7 <songyuqi7@gmail.com>
2020-03-04 13:24:11 +0800

    add random quick sort

--- c7_quick_sort/quick_sort.py
+++ c7_quick_sort/quick_sort.py
@@ -26,7 +26,7 @@ def partition(array, p, r):
     return i + 1
 
 
-#递减排序
+# 递减排序
 def partition_by_desc(array, p, r):
     x = array[r]
     i = p - 1
@@ -38,10 +38,38 @@ def partition_by_desc(array, p, r):
     return i + 1
 
 
+def randomized_partition(array, p, r):
+    """
+        随机选择主元版
+    """
+    i = random_utils.random_int(p, r)
+    array_utils.exchange(array, i, r)
+    return partition(array, p, r)
+
+
+def randomized_quick_sort(array, p, r):
+    if p < r:
+        q = randomized_partition(array, p, r)
+        quick_sort(array, p, q - 1)
+        quick_sort(array, q + 1, r)
+
+
 class QuickSort(unittest.TestCase):
 
-    @staticmethod
-    def test_quick_sort():
-        array = random_utils.random_int_list()
+    def test_quick_sort(self):
+        array = [6, 2, -4, 12, 65, 2]
         quick_sort(array, 0, len(array) - 1)
-        print(array)
+        self.assertEqual(-4, array[0])
+        self.assertEqual(2, array[1])
+        self.assertEqual(65, array[5])
+
+    def test_randomized_quick_sort(self):
+        array = [6, 2, -4, 12, 65, 2]
+        randomized_quick_sort(array, 0, len(array) - 1)
+        self.assertEqual(-4, array[0])
+        self.assertEqual(2, array[1])
+        self.assertEqual(65, array[5])
+
+
+if __name__ == '__main__':
+    unittest.main()
